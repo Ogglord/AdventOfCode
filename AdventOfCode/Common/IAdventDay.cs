dccb35d21a3ac5bf338e33730448b635c6d237fa
Ogglord <oggelito@gmail.com>
2022-11-22 12:01:59 +0100

    cleanup

--- AdventOfCode/Common/IAdventDay.cs
+++ AdventOfCode/Common/IAdventDay.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 namespace AdventOf.Code
 {
+	/// <summary>
+	/// This represents any advent day solution
+	/// </summary>
 	public interface IAdventDay
 	{
 		object PartOne(string input);
