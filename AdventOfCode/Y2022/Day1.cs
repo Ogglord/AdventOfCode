cef5a2384bc43c97b40a6c3da3848b3aa5dadf15
Ogglord <oggelito@gmail.com>
2022-12-01 09:01:51 +0100

    day one 2022

--- AdventOfCode/Y2022/Day1.cs
+++ AdventOfCode/Y2022/Day1.cs
@@ -6,24 +6,24 @@ public class Day1 : IAdventDay
 {
 	private (int partOne, int partTwo) Solve(string input)
 	{
-var elfBackpacks = new List<int>();
-var currElf = 0;
-input
-	.ToLines(removeEmpty: false)
-	.ToList()
-	.ForEach((val) =>
-	{
-		if (val == "")
-		{
-			elfBackpacks.Add((currElf));
-			currElf = 0;
-		}else
-			currElf += Int32.Parse(val);
+		var elfBackpacks = new List<int>();
+		var currElf = 0;
+		input
+			.ToLines(removeEmpty: false)
+			.ToList()
+			.ForEach((val) =>
+			{
+				if (val == "")
+				{
+					elfBackpacks.Add((currElf));
+					currElf = 0;
+				}else
+					currElf += Int32.Parse(val);
 
-	});
+			});
 
-var partOne = elfBackpacks.Max();
-var partTwo = elfBackpacks.OrderDescending().Take(3).Sum();
+		var partOne = elfBackpacks.Max();
+		var partTwo = elfBackpacks.OrderDescending().Take(3).Sum();
 
 		return (partOne, partTwo);
 	}
