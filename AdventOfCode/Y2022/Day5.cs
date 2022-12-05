7b93868f47457913f81847cbf3635aa1626c9aa6
Ogglord <oag@proton.me>
2022-12-05 21:52:51 +0100

    day5 not completed

---
+++ AdventOfCode/Y2022/Day5.cs
@@ -0,0 +1,64 @@
+﻿using AdventOf.Code.Common;
+
+namespace AdventOf.Code.Y2022;
+
+[AdventYear(2022)]
+public class Day5 : IAdventDay
+{
+
+
+
+	private (int partOne, int partTwo) Solve(string input)
+	{
+		List<Stack<char>>? stacks = null;
+
+		var strRead = new StringReader(input);
+		string? line = null;
+
+		while(strRead.Peek() != '\n') {
+			line = strRead.ReadLine()!;
+			if (stacks == null)
+			{
+				var numOfStacks = (line.Length + 1) / 4;
+                stacks = Enumerable
+                    .Range(0, numOfStacks)
+					.Select(i => new Stack<char>())
+					.ToList();
+
+            }
+			// populate the stacks
+			line
+				.Chunk(4)
+				.Select((ch, idx) => (index: idx, crate: ch[1]))
+				.ToList()
+				.ForEach(tpl => stacks[tpl.index].Prepend(tpl.crate));
+		}
+
+		// remove empty crates
+		//stacks!.ForEach(s => {
+		//	var crates = new String(s.ToArray()).Trim().ToList().ForEach( s.p)
+		//		s.Pop();
+		//});
+		// process move instructions
+		while((line = strRead.ReadLine()) != null)
+		{
+			(int nbr, int from, int to) = (line[5], line[12], line[17]);
+			for(int i=nbr; i>0; i--)
+			{
+				stacks[to].Push(stacks[from].Pop());
+			}
+		}
+
+		return (0, 0);
+	}
+
+
+    public object PartOne(string input)
+	{
+		return Solve(input).partOne;
+	}
+		public object PartTwo(string input)
+    {
+		return Solve(input).partTwo;
+	}
+}
---
+++ UnitTests/Y2022/Day5.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOf.Test.y2022;
+
+using AoCHelper;
+using Xunit.Abstractions;
+using AdventOf.Code.Y2022;
+
+public class TestDay5 : BaseTestDay<Day5>
+{
+    public TestDay5(ITestOutputHelper output, TestHelperFixture fixture) : base(output, fixture)
+    {
+    }
+
+	[Fact]  
+	public void PartOneTwo_Test()
+	{
+        var input = "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2";
+		Assert.NotNull(input);
+
+		// Run
+		var result = day.PartOne(input);
+
+		// Write result
+		output.WriteLine($"Result from part one test: {result}");
+
+        // Run
+        var result2 = day.PartTwo(input);
+
+        // Write result
+        output.WriteLine($"Result from part two test: {result2}");
+
+    }
+
+	[Fact]
+    public async void PartOne()
+    {
+        var input = await fixture.GetInput(dayOfAdvent, year);
+        Assert.NotNull(input);
+
+        // Run
+        var result = day.PartOne(input);
+
+        // Write result
+        output.WriteLine($"Result from part one: {result}");
+        
+       
+    }
+
+  
+
+    [Fact]
+    public async void PartTwo()
+    {
+        var input = await fixture.GetInput(dayOfAdvent, year);
+        Assert.NotNull(input);
+
+        // Run
+        var result = day.PartTwo(input);
+
+        // Write result
+        output.WriteLine($"Result from part two: {result}");
+    }
+}
